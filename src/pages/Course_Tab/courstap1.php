49cf9f5c6dcb9dda3582c906bc1f4b8e6ecbfd81
Ganna Mohamed Kamal <g.mahmoud4531@su.edu.eg>
2026-05-07 05:03:17 +0300

    new-edit2

--- src/pages/Course_Tab/courstap1.php
+++ src/pages/Course_Tab/courstap1.php
@@ -12,166 +12,168 @@
 </head>
 
 <body>
-    <div class="card1">
-        <nav class="navbar_dash">
-            <img class="nav-logo" src="../../assets/LogoLa.png" />
+    <div class="dashboard">
+        <div class="card1">
+            <nav class="navbar_dash">
+                <img class="nav-logo" src="../../assets/LogoLa.png" />
 
-            <input type="text" placeholder="Search here..." class="search-bar" />
+                <input type="text" placeholder="Search here..." class="search-bar" />
 
-            <img
-                src="../../assets/search button.png"
-                alt="Search"
-                class="search-button" />
+                <img
+                    src="../../assets/search button.png"
+                    alt="Search"
+                    class="search-button" />
 
-            <div class="score">
-                <h2>Score : 1000 ✦</h2>
+                <div class="score">
+                    <h2>Score : 1000 ✦</h2>
+                </div>
+
+                <button class="theme_btn">
+                    <img src="../../assets/Theme Button.png" />
+                </button>
+            </nav>
+
+            <div class="tabs">
+                <span class="active">All(5)</span>
+                <span>Completed(1)</span>
             </div>
 
-            <button class="theme_btn">
-                <img src="../../assets/Theme Button.png" />
-            </button>
-        </nav>
+            <div class="courses-grid">
+                <div class="card">
+                    <div id="box" class="card-image bg-anatomy">
 
-        <div class="tabs">
-            <span class="active">All(5)</span>
-            <span>Completed(1)</span>
+                        <h3>Anatomy</h3>
+                    </div>
+                    <div class="card-content">
+                        <div class="stats">
+                            <span><i class="fa-solid fa-square-poll-vertical"></i> 3 Quizzes</span>
+                            <span><i class="fa-solid fa-book-bookmark"></i> 32 Cases</span>
+                        </div>
+                        <p class="description"><strong>Description:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit... <a href="#">see more</a></p>
+                        <div class="progress-container">
+                            <div class="progress-bar">
+                                <div class="progress" style="width: 0%;"></div>
+                            </div>
+                            <span class="percentage">0%</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-image bg-test">
+                        <h3>Test and Measurements</h3>
+                    </div>
+                    <div class="card-content">
+                        <div class="stats">
+                            <span><i class="fa-solid fa-square-poll-vertical"></i> 3 Quizzes</span>
+                            <span><i class="fa-solid fa-book-bookmark"></i> 32 Cases</span>
+                        </div>
+                        <p class="description"><strong>Description:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit... <a href="#">see more</a></p>
+                        <div class="progress-container">
+                            <div class="progress-bar">
+                                <div class="progress" style="width: 0%;"></div>
+                            </div>
+                            <span class="percentage">0%</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-image bg-therapeutic">
+                        <h3>Therapeutic Exercises</h3>
+                    </div>
+                    <div class="card-content">
+                        <div class="stats">
+                            <span><i class="fa-solid fa-square-poll-vertical"></i> 3 Quizzes</span>
+                            <span><i class="fa-solid fa-book-bookmark"></i> 32 Cases</span>
+                        </div>
+                        <p class="description"><strong>Description:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit... <a href="#">see more</a></p>
+                        <div class="progress-container">
+                            <div class="progress-bar">
+                                <div class="progress" style="width: 0%;"></div>
+                            </div>
+                            <span class="percentage">0%</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-image bg-hydro">
+                        <h3>Hydro Therapy</h3>
+                    </div>
+                    <div class="card-content">
+                        <div class="stats">
+                            <span><i class="fa-solid fa-square-poll-vertical"></i> 3 Quizzes</span>
+                            <span><i class="fa-solid fa-book-bookmark"></i> 32 Cases</span>
+                        </div>
+                        <p class="description"><strong>Description:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit... <a href="#">see more</a></p>
+                        <div class="progress-container">
+                            <div class="progress-bar">
+                                <div class="progress complete" style="width: 100%;"></div>
+                            </div>
+                            <span class="percentage">100%</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card locked">
+                    <div class="lock-overlay">
+                        <i class="fa-solid fa-lock"></i>
+                        <p>Need to reach 20000 score to unlock this Course</p>
+                    </div>
+                    <div class="card-image bg-manual">
+                        <h3>Manual Therapy</h3>
+                    </div>
+                    <div class="card-content">
+                        <div class="stats">
+                            <span><i class="fa-solid fa-square-poll-vertical"></i> 3 Quizzes</span>
+                            <span><i class="fa-solid fa-book-bookmark"></i> 32 Cases</span>
+                        </div>
+                        <p class="description"><strong>Description:</strong> Lorem ipsum dolor sit amet... <a href="#">see more</a></p>
+                        <div class="progress-container">
+                            <div class="progress-bar">
+                                <div class="progress" style="width: 0%;"></div>
+                            </div>
+                            <span class="percentage">0%</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
-        <div class="courses-grid">
-            <div class="card">
-                <div id="box" class="card-image bg-anatomy">
-
-                    <h3>Anatomy</h3>
-                </div>
-                <div class="card-content">
-                    <div class="stats">
-                        <span><i class="fa-solid fa-square-poll-vertical"></i> 3 Quizzes</span>
-                        <span><i class="fa-solid fa-book-bookmark"></i> 32 Cases</span>
-                    </div>
-                    <p class="description"><strong>Description:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit... <a href="#">see more</a></p>
-                    <div class="progress-container">
-                        <div class="progress-bar">
-                            <div class="progress" style="width: 0%;"></div>
-                        </div>
-                        <span class="percentage">0%</span>
-                    </div>
-                </div>
+        <div class="sidebar">
+            <div class="profile">
+                <button onclick="location.href='../profile_tab/profile.html'">
+                    <img src="../../assets/profile.png" alt="Profile">
+                    <p><b>Profile Name</b></p>
+                </button>
             </div>
 
-            <div class="card">
-                <div class="card-image bg-test">
-                    <h3>Test and Measurements</h3>
+            <div class="Menu">
+                <p class="MM">MAIN MENU</p>
+                <div class="menu-items">
+                    <p class="course"
+                        onclick="location.href = '../dashboard/dashboard.php'"><img src="../../assets/dashboard icon.png" alt="icon"> Dashboard</p>
+                    <p><img src="../../assets/courses icon.png" alt="icon"> Courses</p>
+                    <p><img src="../../assets/quizes icon.png" alt="icon"> Quizes</p>
+                    <span><img src="../../assets/setting icon.png" alt="icon"> Settings</span>
                 </div>
-                <div class="card-content">
-                    <div class="stats">
-                        <span><i class="fa-solid fa-square-poll-vertical"></i> 3 Quizzes</span>
-                        <span><i class="fa-solid fa-book-bookmark"></i> 32 Cases</span>
-                    </div>
-                    <p class="description"><strong>Description:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit... <a href="#">see more</a></p>
-                    <div class="progress-container">
-                        <div class="progress-bar">
-                            <div class="progress" style="width: 0%;"></div>
-                        </div>
-                        <span class="percentage">0%</span>
-                    </div>
-                </div>
-            </div>
 
-            <div class="card">
-                <div class="card-image bg-therapeutic">
-                    <h3>Therapeutic Exercises</h3>
+                <p class="RA">RECENT ACHIEVEMENTS</p>
+                <div class="img-achievements">
+                    <img src="../../assets/Achievement.png" alt="Achievement 2">
+                    <img src="../../assets/Achievement (1).png" alt="Achievement 1">
                 </div>
-                <div class="card-content">
-                    <div class="stats">
-                        <span><i class="fa-solid fa-square-poll-vertical"></i> 3 Quizzes</span>
-                        <span><i class="fa-solid fa-book-bookmark"></i> 32 Cases</span>
-                    </div>
-                    <p class="description"><strong>Description:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit... <a href="#">see more</a></p>
-                    <div class="progress-container">
-                        <div class="progress-bar">
-                            <div class="progress" style="width: 0%;"></div>
-                        </div>
-                        <span class="percentage">0%</span>
-                    </div>
-                </div>
-            </div>
-
-            <div class="card">
-                <div class="card-image bg-hydro">
-                    <h3>Hydro Therapy</h3>
-                </div>
-                <div class="card-content">
-                    <div class="stats">
-                        <span><i class="fa-solid fa-square-poll-vertical"></i> 3 Quizzes</span>
-                        <span><i class="fa-solid fa-book-bookmark"></i> 32 Cases</span>
-                    </div>
-                    <p class="description"><strong>Description:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit... <a href="#">see more</a></p>
-                    <div class="progress-container">
-                        <div class="progress-bar">
-                            <div class="progress complete" style="width: 100%;"></div>
-                        </div>
-                        <span class="percentage">100%</span>
-                    </div>
-                </div>
-            </div>
-
-            <div class="card locked">
-                <div class="lock-overlay">
-                    <i class="fa-solid fa-lock"></i>
-                    <p>Need to reach 20000 score to unlock this Course</p>
-                </div>
-                <div class="card-image bg-manual">
-                    <h3>Manual Therapy</h3>
-                </div>
-                <div class="card-content">
-                    <div class="stats">
-                        <span><i class="fa-solid fa-square-poll-vertical"></i> 3 Quizzes</span>
-                        <span><i class="fa-solid fa-book-bookmark"></i> 32 Cases</span>
-                    </div>
-                    <p class="description"><strong>Description:</strong> Lorem ipsum dolor sit amet... <a href="#">see more</a></p>
-                    <div class="progress-container">
-                        <div class="progress-bar">
-                            <div class="progress" style="width: 0%;"></div>
-                        </div>
-                        <span class="percentage">0%</span>
-                    </div>
-                </div>
+                <p class="discover"> DISCOVER MODE</p>
+                <p class="full-body"><img src="../../assets/full-body-icon.png" alt="icon">Full Body
+                    <img src="../../assets/arrow_drop_down.png" alt="icon">
+                </p>
+                <p class="Account">ACCOUNT </p>
+                <button class="Log-Out" onclick="location.href = '../landing/landing.php'">
+                    <p class="Log-Out"><img src="../../assets/Log Out.png" alt="icon">Log Out</p>
+                </button>
             </div>
         </div>
-    </div>
-
-    <div class="sidebar">
-        <div class="profile">
-            <button onclick="location.href='../profile_tab/profile.html'">
-                <img src="../../assets/profile.png" alt="Profile">
-                <p><b>Profile Name</b></p>
-            </button>
-        </div>
-
-        <div class="Menu">
-            <p class="MM">MAIN MENU</p>
-            <div class="menu-items">
-                <p><img src="../../assets/dashboard icon.png" alt="icon"> Dashboard</p>
-                <p><img src="../../assets/courses icon.png" alt="icon"> Courses</p>
-                <p><img src="../../assets/quizes icon.png" alt="icon"> Quizes</p>
-                <span><img src="../../assets/setting icon.png" alt="icon"> Settings</span>
-            </div>
-
-            <p class="RA">RECENT ACHIEVEMENTS</p>
-            <div class="img-achievements">
-                <img src="../../assets/Achievement.png" alt="Achievement 2">
-                <img src="../../assets/Achievement (1).png" alt="Achievement 1">
-            </div>
-            <p class="discover"> DISCOVER MODE</p>
-            <p class="full-body"><img src="../../assets/full-body-icon.png" alt="icon">Full Body
-                <img src="../../assets/arrow_drop_down.png" alt="icon">
-            </p>
-            <p class="Account">ACCOUNT </p>
-            <button class="Log-Out" onclick="location.href = '../landing/landing.php'">
-                <p class="Log-Out"><img src="../../assets/Log Out.png" alt="icon">Log Out</p>
-            </button>
-        </div>
-    </div>
     </div>
 
 
--- src/pages/auth/sign_up.php
+++ src/pages/auth/sign_up.php
@@ -4,57 +4,59 @@ $connection = new mysqli("localhost", "root", "", "diagnosis");
 $error = "";
 
 if (isset($_POST['username'])) {
-    $username   = $_POST['username'];
-    $email      = $_POST['email'];
-    $password   = $_POST['password'];
-    $major      = $_POST['major'];
-    $level      = $_POST['level'];
-    $created_at = $_POST['created_at'] ?? date("Y-m-d H:i:s");
+  $username   = $_POST['username'];
+  $email      = $_POST['email'];
+  $password   = $_POST['password'];
+  $major      = $_POST['major'];
+  $level      = $_POST['level'];
+  $created_at = $_POST['created_at'] ?? date("Y-m-d H:i:s");
 
-    // Check username
-    $check = $connection->prepare("SELECT id FROM users WHERE username = ?");
-    $check->bind_param("s", $username);
-    $check->execute();
-    $result = $check->get_result();
-    if ($result->fetch_assoc()) {
-        $error = "Username already exists.";
-    }
+  // Check username
+  $check = $connection->prepare("SELECT id FROM users WHERE username = ?");
+  $check->bind_param("s", $username);
+  $check->execute();
+  $result = $check->get_result();
+  if ($result->fetch_assoc()) {
+    $error = "Username already exists.";
+  }
 
-    // Check email
-    if (!$error) {
-        $check2 = $connection->prepare("SELECT id FROM users WHERE email = ?");
-        $check2->bind_param("s", $email);
-        $check2->execute();
-        $result2 = $check2->get_result();
-        if ($result2->fetch_assoc()) {
-            $error = "Email already registered.";
-        }
-    }
-
-    if (!$error) {
-        $hashed = password_hash($password, PASSWORD_BCRYPT);
-        $add = $connection->prepare("INSERT INTO users (username, email, password, major, level, created_at) VALUES (?, ?, ?, ?, ?, ?)");
-        $add->bind_param("ssssss", $username, $email, $hashed, $major, $level, $created_at);
-        $add->execute();
-        header("Location: login.php?registered=1");
-        exit;
+  // Check email
+  if (!$error) {
+    $check2 = $connection->prepare("SELECT id FROM users WHERE email = ?");
+    $check2->bind_param("s", $email);
+    $check2->execute();
+    $result2 = $check2->get_result();
+    if ($result2->fetch_assoc()) {
+      $error = "Email already registered.";
     }
   }
+
+  if (!$error) {
+    $hashed = password_hash($password, PASSWORD_BCRYPT);
+    $add = $connection->prepare("INSERT INTO users (username, email, password, major, level, created_at) VALUES (?, ?, ?, ?, ?, ?)");
+    $add->bind_param("ssssss", $username, $email, $hashed, $major, $level, $created_at);
+    $add->execute();
+    header("Location: login.php?registered=1");
+    exit;
+  }
+}
 ?>
 
 <!doctype html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8" />
   <title>Diagnosis Master Sign Up</title>
   <link rel="stylesheet" href="style_sign_up.css" />
   <link rel="stylesheet" href="styles/common.css" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
 </head>
 
 <body>
   <div class="container">
-
     <div class="left">
       <img class="logo" src="../../assets/Logo.png" alt="" />
     </div>
@@ -69,34 +71,49 @@ if (isset($_POST['username'])) {
 
         <h2>Sign Up</h2>
 
-        <form method="post" action="" id="signupForm">
-
+        <form method="post" action="sign_up.php" id="signupForm">
           <input type="hidden" id="created_at" name="created_at" />
 
           <!-- Username -->
           <label><i class="fa-regular fa-user"></i>&nbsp;<b>Username</b></label>
           <div class="input-box">
-            <input type="text" id="username" name="username" placeholder="Username"
+            <input
+              type="text"
+              id="username"
+              name="username"
+              placeholder="Username"
               value="<?= htmlspecialchars($_POST['username'] ?? '') ?>" />
           </div>
 
           <!-- Email -->
           <label><i class="fa-solid fa-envelope"></i>&nbsp;<b>Email</b></label>
           <div class="input-box">
-            <input type="email" id="email" name="email" placeholder="Email"
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email"
               value="<?= htmlspecialchars($_POST['email'] ?? '') ?>" />
           </div>
 
           <!-- Password -->
           <label><i class="fa-solid fa-lock"></i>&nbsp;<b>Password</b></label>
           <div class="input-box">
-            <input type="password" id="password" name="password" placeholder="Password" />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password" />
           </div>
 
           <!-- Confirm Password -->
           <label><i class="fa-solid fa-lock"></i>&nbsp;<b>Confirm Password</b></label>
           <div class="input-box">
-            <input type="password" id="confirm_password" name="confirm_password" placeholder="Confirm Password" />
+            <input
+              type="password"
+              id="confirm_password"
+              name="confirm_password"
+              placeholder="Confirm Password" />
           </div>
 
           <!-- Major & Level -->
@@ -119,17 +136,16 @@ if (isset($_POST['username'])) {
             </div>
           </div>
 
-          <br>
-
           <!-- error + button wrapper -->
           <div class="error-wrapper">
             <div id="error-box"></div>
             <?php if ($error) { ?>
               <div class="php-error"><?= $error ?></div>
             <?php } ?>
-            <button class="btn-signup" type="button" onclick="validateForm()">Sign Up</button>
+            <button class="btn-signup" type="button" onclick="validateForm()">
+              Sign Up
+            </button>
           </div>
-
         </form>
       </div>
     </div>
@@ -138,52 +154,64 @@ if (isset($_POST['username'])) {
   <script>
     window.onload = function() {
       var now = new Date();
-      var formatted = now.getFullYear() + '-' +
-        String(now.getMonth() + 1).padStart(2, '0') + '-' +
-        String(now.getDate()).padStart(2, '0') + ' ' +
-        String(now.getHours()).padStart(2, '0') + ':' +
-        String(now.getMinutes()).padStart(2, '0') + ':' +
-        String(now.getSeconds()).padStart(2, '0');
-      document.getElementById('created_at').value = formatted;
+      var formatted =
+        now.getFullYear() +
+        "-" +
+        String(now.getMonth() + 1).padStart(2, "0") +
+        "-" +
+        String(now.getDate()).padStart(2, "0") +
+        " " +
+        String(now.getHours()).padStart(2, "0") +
+        ":" +
+        String(now.getMinutes()).padStart(2, "0") +
+        ":" +
+        String(now.getSeconds()).padStart(2, "0");
+      document.getElementById("created_at").value = formatted;
     };
 
     function validateForm() {
       var errors = [];
 
-      var username = document.getElementById('username').value.trim();
-      var email    = document.getElementById('email').value.trim();
-      var password = document.getElementById('password').value;
-      var confirm  = document.getElementById('confirm_password').value;
-      var major    = document.getElementById('major').value;
-      var level    = document.getElementById('level').value;
+      var username = document.getElementById("username").value.trim();
+      var email = document.getElementById("email").value.trim();
+      var password = document.getElementById("password").value;
+      var confirm = document.getElementById("confirm_password").value;
+      var major = document.getElementById("major").value;
+      var level = document.getElementById("level").value;
 
-      if (username === '')                                    errors.push('Username is required.');
-      else if (username.length < 3)                          errors.push('Username must be at least 3 characters.');
-      else if (!/^[a-zA-Z0-9_]+$/.test(username))           errors.push('Username: only letters, numbers, underscores.');
+      if (username === "") errors.push("Username is required.");
+      else if (username.length < 3)
+        errors.push("Username must be at least 3 characters.");
+      else if (!/^[a-zA-Z0-9_]+$/.test(username))
+        errors.push("Username: only letters, numbers, underscores.");
 
-      if (email === '')                                       errors.push('Email is required.');
-      else if (!email.includes('@') || !email.includes('.')) errors.push('Invalid email format.');
+      if (email === "") errors.push("Email is required.");
+      else if (!email.includes("@") || !email.includes("."))
+        errors.push("Invalid email format.");
 
-      if (password === '')               errors.push('Password is required.');
-      else if (password.length < 8)     errors.push('Password must be at least 8 characters.');
-      else if (!/[0-9]/.test(password)) errors.push('Password must contain a number.');
-      else if (!/[A-Z]/.test(password)) errors.push('Password must contain an uppercase letter.');
+      if (password === "") errors.push("Password is required.");
+      else if (password.length < 8)
+        errors.push("Password must be at least 8 characters.");
+      else if (!/[0-9]/.test(password))
+        errors.push("Password must contain a number.");
+      else if (!/[A-Z]/.test(password))
+        errors.push("Password must contain an uppercase letter.");
 
-      if (confirm === '')            errors.push('Please confirm your password.');
-      else if (password !== confirm) errors.push('Passwords do not match.');
+      if (confirm === "") errors.push("Please confirm your password.");
+      else if (password !== confirm) errors.push("Passwords do not match.");
 
-      if (major === '') errors.push('Please select a major.');
-      if (level === '') errors.push('Please select a level.');
+      if (major === "") errors.push("Please select a major.");
+      if (level === "") errors.push("Please select a level.");
 
-      var box = document.getElementById('error-box');
+      var box = document.getElementById("error-box");
       if (errors.length > 0) {
-        box.innerText = errors.join('\n');
+        box.innerText = errors.join(" ");
       } else {
-        box.innerText = '';
-        document.getElementById('signupForm').submit();
+        box.innerText = "";
+        document.getElementById("signupForm").submit();
       }
     }
   </script>
-
 </body>
+
 </html>
